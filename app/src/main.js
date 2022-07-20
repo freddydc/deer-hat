7e536bdffc84c1a5d30a357b5ca4b86926392f3a
freddydc <freddydiaz.liv@gmail.com>
2022-07-20 12:30:21 -0500

    Query users

--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,18 @@
 import '../styles/general.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <button>Look</button>
+  </div>
+`
+
+const handleQuery = document.querySelector('button')
+handleQuery.addEventListener('click', getUsers)
+
+function getUsers() {
+  fetch('/api/v1/users')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+}
