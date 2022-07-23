3cf02f13ad1221cf1f7f777f69d083eada57e3bc
freddydc <freddydiaz.liv@gmail.com>
2022-07-23 17:53:03 -0500

    Static files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import { router } from './router.js'
+import path from 'path'
 
 const app = express()
 const port = 8000
 
+const CURRENT_DIR = path.resolve()
+const STATIC_DIR = 'app/dist'
+
 router(app)
+
+app.use('/', express.static(path.join(CURRENT_DIR, STATIC_DIR)))
 
 app.listen(port, () => {
   console.log(`🚀 Server at http://localhost:${port}`)
